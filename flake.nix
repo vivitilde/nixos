4901716decfe2b0bf1d09286251db9c728c42c8b
dark star <12040089+vivitilde@users.noreply.github.com>
2025-11-04 17:55:22 -0500

    set up file structure
    not working yet

--- flake.nix
+++ flake.nix
@@ -3,7 +3,6 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-quartus.url = "github:nixos/nixpkgs/nixos-22.05";
 
      home-manager = {
        url = "github:nix-community/home-manager";
@@ -11,19 +10,25 @@
      };
   };
 
-  outputs = { self, nixpkgs, nixpkgs-quartus, ... }@inputs:
+  outputs = { self, nixpkgs, ... }@inputs:
   let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
-    oldpkgs = inputs.nixpkgs-quartus.legacyPackages.${system};
   in
   {
     nixosConfigurations = {
       astra = nixpkgs.lib.nixosSystem {
         specialArgs = { inherit inputs ; };
         modules = [
-          ./configuration.nix
+          ./hosts/astra/configuration.nix
           inputs.home-manager.nixosModules.default
+        ];
+      };
+      umbra = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs ; };
+	modules = [
+	  ./host/umbra/configuration.nix
+	  inputs.home-manager.nixosModudles.default
         ];
       };
     };
