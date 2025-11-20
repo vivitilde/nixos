54be3f117b8507c90c6f78157a990d9f5268bdc7
dark star <12040089+vivitilde@users.noreply.github.com>
2025-11-19 20:00:41 -0500

    Create spotify.nix

---
+++ modules/spotify.nix
@@ -0,0 +1,18 @@
+{ pkgs, inputs, ...}: {
+  # spicetify setup for spotify
+  programs.spicetify =
+  let
+    spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.stdenv.hostPlatform.system};
+  in
+  {
+    enable = true;
+    theme = spicePkgs.themes.text;
+    enabledExtensions = with spicePkgs.extensions; [
+      adblockify
+      hidePodcasts
+      shuffle
+      keyboardShortcut
+    ];
+  };
+
+  
