3551916f99392b8ec6cf32106099ee654b7f52e6
dark star <12040089+vivitilde@users.noreply.github.com>
2025-11-21 14:56:40 -0500

    fixed spotify/spicetify and added virutal box to umbra

--- hosts/umbra/configuration.nix
+++ hosts/umbra/configuration.nix
@@ -2,14 +2,14 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ config, pkgs, inputs, ... }:
 
 {
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
       ../../modules/spotify.nix
-      
+      inputs.spicetify-nix.nixosModules.spicetify
     ];
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
@@ -91,6 +91,7 @@
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
       kdePackages.kate
+      virtualbox
     #  thunderbird
     ];
   };
