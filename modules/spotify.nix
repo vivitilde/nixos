0f8e5622d26b3ff4690c6955a5658985bb01dbc0
dark star <12040089+vivitilde@users.noreply.github.com>
2025-11-21 05:17:08 -0500

    added spotify to the spotify module @_@

--- modules/spotify.nix
+++ modules/spotify.nix
@@ -1,5 +1,9 @@
 { config, pkgs, inputs, spicePkgs, ...}: {
  # spicetify setup for spotify
+
+  environment.systemPackages = with pkgs; [ 
+    spotify
+  ];
   programs.spicetify =
   let
     spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.stdenv.hostPlatform.system};
